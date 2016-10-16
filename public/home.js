ad0ad4c77513b39e68f8019c8c6490a339440e51
ama54 <anthonymasencio@gmail.com>
2016-10-15 23:01:40 -0700

    moved js and deleted skeleton code

--- public/home.js
+++ public/home.js
@@ -3,9 +3,22 @@
 
 	// instructions to load the page
 	window.onload = function() {
+		initialize();
 		ID("search").onclick = search;
 	};
 	
+	function initialize() {
+		// Initialize Firebase
+		  var config = {
+		    apiKey: "AIzaSyBYY-2NRwIHT8KLGUz0NResAmBQH0o_J8U",
+		    authDomain: "hackathon2016-16636.firebaseapp.com",
+		    databaseURL: "https://hackathon2016-16636.firebaseio.com",
+		    storageBucket: "hackathon2016-16636.appspot.com",
+		    messagingSenderId: "521305151"
+		  };
+		  firebase.initializeApp(config);
+	}
+
 	function search() {
 		var value = ID("searchbar").value;
 		//alert(value);
