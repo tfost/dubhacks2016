d7fcad44c9d5405a587d02183f825c04bf2f8b82
Tyler Fost <tyler.fost@gmail.com>
2016-10-15 22:23:18 -0700

    Created initial firebase support

--- public/index.js
+++ public/index.js
@@ -8,8 +8,15 @@
 	
 	function search() {
 		var value = ID("searchbar").value;
-		alert(value);
+		//alert(value);
+		console.log("value = " + value);
+		var database = firebase.database();
+		firebase.database().ref('users/' + 1).set({
+			name : value
+		});
+		console.log("finished writing value");
 		// your crap goes here
+
 	}
 	
 	// returns an element of a given id
